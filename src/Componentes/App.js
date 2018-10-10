756a9af331efd94e77fc667bd310b977577797c5
natalia95ba <43654660+natalia95ba@users.noreply.github.com>
2018-10-10 12:42:42 +0200

    Update App.js

--- src/Componentes/App.js
+++ src/Componentes/App.js
@@ -5,13 +5,6 @@ import FormularioGasto from './Formulario';
 import Listado from './Listado';
 
 class App extends Component {
-
-  /* puede crearse el state asi: 
-  constructor(props) {
-  super(props);
-  this.sate = {
-    gastos:{}
-  } */
   state = {
     presupuesto: '',
     restante: '',
@@ -21,22 +14,15 @@ class App extends Component {
 agregarGasto = gasto => {
 
 //tomar una copia del state actual (hay vecez que el state 
-//esta vacío pero otras No y por eso la copia), lo 
-//lo hacemos con spread operate
+
 const gastos = {...this.state.gastos};
 
 //agregar el gasto al objeto y enviarlo por props
-//para eso lo hacemos con una llave
-//trabajamos con gastos por ahi almacenamos la copia
-//[`gasto${Date.now()}`] es para poner el gasto con fecha
 gastos[`gasto${Date.now()}`] = gasto;
 
 console.log(gastos);
 
-
-
 //ponerlo en el state
-//siempre para agregar algo a state será con setState
 this.setState({
   gastos : gastos
 })
